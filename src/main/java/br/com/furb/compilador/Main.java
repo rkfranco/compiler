e630e3248de3f8b9365bb57002acd65e164a934f
gustavobruder <gugabruder@gmail.com>
2022-08-27 17:46:42 -0300

    Adicionado teclas de atalho aos botões da interface

--- src/main/java/br/com/furb/compilador/Main.java
+++ src/main/java/br/com/furb/compilador/Main.java
@@ -3,6 +3,7 @@ package br.com.furb.compilador;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,13 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 910, 600);
+
+        ViewController controller = fxmlLoader.getController();
+        scene.addEventFilter(KeyEvent.KEY_RELEASED, event -> controller.handleKeyEvents(event));
+
         stage.setTitle("FURB - Compilador");
+        stage.setMinWidth(910);
+        stage.setMinHeight(600);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/br/com/furb/compilador/ViewController.java
+++ src/main/java/br/com/furb/compilador/ViewController.java
@@ -6,6 +6,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
 
 public class ViewController {
@@ -82,5 +86,41 @@ public class ViewController {
 
     public void onBtTeamAction() {
         System.out.println("Equipe");
+    }
+
+    private final KeyCombination keyBtNew = new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_DOWN);
+    private final KeyCombination keyBtOpen = new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_DOWN);
+    private final KeyCombination keyBtSave = new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_DOWN);
+    private final KeyCombination keyBtCopy = new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_DOWN);
+    private final KeyCombination keyBtPaste = new KeyCodeCombination(KeyCode.V, KeyCombination.CONTROL_DOWN);
+    private final KeyCombination keyBtCut = new KeyCodeCombination(KeyCode.X, KeyCombination.CONTROL_DOWN);
+    private final KeyCode keyBtCompile = KeyCode.F7;
+    private final KeyCode keyBtTeam = KeyCode.F1;
+
+    public void handleKeyEvents(KeyEvent event) {
+        if (this.keyBtNew.match(event)) {
+            this.onBtNewAction();
+        }
+        if (this.keyBtOpen.match(event)) {
+            this.onBtOpenAction();
+        }
+        if (this.keyBtSave.match(event)) {
+            this.onBtSaveAction();
+        }
+        if (this.keyBtCopy.match(event)) {
+            this.onBtCopyAction();
+        }
+        if (this.keyBtPaste.match(event)) {
+            this.onBtPasteAction();
+        }
+        if (this.keyBtCut.match(event)) {
+            this.onBtCutAction();
+        }
+        if (event.getCode().equals(this.keyBtCompile)) {
+            this.onBtCompileAction();
+        }
+        if (event.getCode().equals(this.keyBtTeam)) {
+            this.onBtTeamAction();
+        }
     }
 }
