1cefb066ae9a4d8e926779707ee167e5858da0f2
Rodrigo Franco <rodrigokafra@hotmail.com>
2022-09-02 16:16:45 -0300

    [fix] Scroll do not disapere when borderpane is in min size

--- src/main/java/br/com/furb/compilador/Main.java
+++ src/main/java/br/com/furb/compilador/Main.java
@@ -12,16 +12,20 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 910, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        scene.getStylesheets().add(getClass().getResource("Style.css").toExternalForm());
 
         ViewController controller = fxmlLoader.getController();
         scene.addEventFilter(KeyEvent.KEY_RELEASED, event -> controller.handleKeyEvents(event));
+
+        System.out.println(scene.getWidth());
 
         stage.setTitle("FURB - Compilador");
         stage.setMinWidth(910);
         stage.setMinHeight(600);
         stage.setScene(scene);
         stage.show();
+        System.out.println(scene.getWidth());
     }
 
     public static void main(String[] args) {
