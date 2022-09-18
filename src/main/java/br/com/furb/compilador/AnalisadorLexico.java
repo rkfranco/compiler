386e326e5a43e9f5183fa22356b5f02be0bb4c0f
gustavobruder <gugabruder@gmail.com>
2022-09-18 18:22:03 -0300

    [feat] Analisador léxico e mensagens de erro

---
+++ src/main/java/br/com/furb/compilador/AnalisadorLexico.java
@@ -0,0 +1,86 @@
+package br.com.furb.compilador;
+
+import br.com.furb.compilador.gals.LexicalError;
+import br.com.furb.compilador.gals.Lexico;
+import br.com.furb.compilador.gals.Token;
+import org.fxmisc.richtext.CodeArea;
+
+import static br.com.furb.compilador.gals.ScannerConstants.SCANNER_ERROR;
+
+public class AnalisadorLexico {
+
+    private final Lexico lexico;
+    private final String quebraLinha;
+
+    public AnalisadorLexico() {
+        this.lexico = new Lexico();
+        this.quebraLinha = System.lineSeparator();
+    }
+
+    public String analisar(CodeArea entrada) {
+        this.lexico.setInput(entrada.getText());
+        StringBuilder saida = new StringBuilder()
+            .append(padRight("linha", 10))
+            .append(padRight("classe", 30))
+            .append("lexema")
+            .append(this.quebraLinha);
+
+        try {
+            Token token;
+            while ((token = this.lexico.nextToken()) != null) {
+                saida
+                    .append(padRight(String.valueOf(obterLinha(entrada, token.getPosition())), 10))
+                    .append(padRight(obterClasse(token.getId()), 30))
+                    .append(token.getLexeme())
+                    .append(this.quebraLinha);
+            }
+            saida.append("programa compilado com sucesso").append(this.quebraLinha);
+        } catch (LexicalError e) {
+            saida = new StringBuilder()
+                .append("Erro na linha ")
+                .append(obterLinha(entrada, e.getPosition()))
+                .append(" - ")
+                .append(formatarMensagemErro(e, entrada.getText()))
+                .append(this.quebraLinha);
+        }
+
+        return saida.toString();
+    }
+
+    private String padRight(String texto, int quantidade) {
+        return String.format("%-" + quantidade + "s", texto);
+    }
+
+    private int obterLinha(CodeArea entrada, int position) {
+        entrada.selectRange(position, position);
+        return entrada.getCaretSelectionBind().getParagraphIndex() + 1;
+    }
+
+    private String obterClasse(int id) {
+        switch (id) {
+            case 2:
+                return "identificador";
+            case 3:
+                return "constante int";
+            case 4:
+                return "constante float";
+            case 5:
+                return "constante char";
+            case 6:
+                return "constante string";
+        }
+        if (id >= 7 && id <= 26) {
+            return "palavra reservada";
+        }
+        if (id > 26) {
+            return "símbolo especial";
+        }
+        return "Ainda não implementado";
+    }
+
+    private String formatarMensagemErro(LexicalError error, String texto) {
+        return SCANNER_ERROR[0].equals(error.getMessage())
+                ? texto.charAt(error.getPosition()) + " " + error.getMessage()
+                : error.getMessage();
+    }
+}
--- src/main/java/br/com/furb/compilador/ViewController.java
+++ src/main/java/br/com/furb/compilador/ViewController.java
@@ -23,6 +23,7 @@ import br.com.furb.compilador.gals.*;
 public class ViewController implements Initializable {
     private String filePath = "";
     private String nameFile;
+    private final AnalisadorLexico analisadorLexico = new AnalisadorLexico();
 
     @FXML
     private CodeArea codeInput;
@@ -175,40 +176,8 @@ public class ViewController implements Initializable {
     public Button btnCompilar;
 
     public void onBtnCompilarAction() {
-        String debugOutput = "Lexema - Class - Linha\n";
-        Lexico lexico = new Lexico();
-        lexico.setInput(codeInput.getText());
-        try {
-            Token t = null;
-            while ((t = lexico.nextToken()) != null) {
-                debugOutput += t.getLexeme() + " - " + getClassById(t.getId()) + " na linha " + getLine(t.getPosition()) + "\n";
-
-                // só escreve o lexema, necessário escrever t.getId, t.getPosition()
-
-                // t.getId () - retorna o identificador da classe. Olhar Constants.java e adaptar, pois
-                // deve ser apresentada a classe por extenso
-                // t.getPosition () - retorna a posição inicial do lexema no editor, necessário adaptar
-                // para mostrar a linha
-
-                // esse código apresenta os tokens enquanto não ocorrer erro
-                // no entanto, os tokens devem ser apresentados SÓ se não ocorrer erro, necessário adaptar
-                // para atender o que foi solicitado
-            }
-        } catch (LexicalError e) {  // tratamento de erros
-            debugOutput = e.getMessage() + " na linha " + getLine(e.getPosition()) + "\n";
-
-            // e.getMessage() - retorna a mensagem de erro de SCANNER_ERRO (olhar ScannerConstants.java
-            // e adaptar conforme o enunciado da parte 2)
-            // e.getPosition() - retorna a posição inicial do erro, tem que adaptar para mostrar a
-            // linha
-        }
-        txtaDebug.appendText(debugOutput);
-    }
-
-    private int getLine(int erroPosition) {
-        codeInput.selectRange(erroPosition, erroPosition);
-        int numberline = codeInput.getCaretSelectionBind().getParagraphIndex() + 1;
-        return numberline;
+        String saida = this.analisadorLexico.analisar(this.codeInput);
+        txtaDebug.appendText(saida);
     }
 
     @FXML
@@ -252,74 +221,5 @@ public class ViewController implements Initializable {
         if (event.getCode().equals(this.keyBtnEquipe)) {
             this.onBtnEquipeAction();
         }
-    }
-    private String getClassById(int id) {
-        switch (id) {
-            case 2:
-                return "identificador";
-            case 3:
-                return "constante int";
-            case 4:
-                return "constante float";
-            case 5:
-                return "constante char";
-            case 6:
-                return "constante string";
-        }
-        if (id >= 7 && id <= 26) {
-            return "palavra reservada";
-        }
-        if (id > 26) {
-            return "símbolo especial";
-        }
-        return "Ainda não implementado";
-
-//        int t_identificador = 2;
-//        int t_cint = 3;
-//        int t_cfloat = 4;
-//        int t_cchar = 5;
-//        int t_cstring = 6;
-//        int t_boolean = 7;
-//        int t_break = 8;
-//        int t_char = 9;
-//        int t_do = 10;
-//        int t_else = 11;
-//        int t_end = 12;
-//        int t_false = 13;
-//        int t_float = 14;
-//        int t_fun = 15;
-//        int t_if = 16;
-//        int t_int = 17;
-//        int t_main = 18;
-//        int t_print = 19;
-//        int t_println = 20;
-//        int t_readln = 21;
-//        int t_string = 22;
-//        int t_true = 23;
-//        int t_val = 24;
-//        int t_var = 25;
-//        int t_while = 26;
-//        int t_TOKEN_27 = 27; //":"
-//        int t_TOKEN_28 = 28; //","
-//        int t_TOKEN_29 = 29; //";"
-//        int t_TOKEN_30 = 30; //"="
-//        int t_TOKEN_31 = 31; //")"
-//        int t_TOKEN_32 = 32; //"("
-//        int t_TOKEN_33 = 33; //"{"
-//        int t_TOKEN_34 = 34; //"}"
-//        int t_TOKEN_35 = 35; //"=="
-//        int t_TOKEN_36 = 36; //"<"
-//        int t_TOKEN_37 = 37; //">"
-//        int t_TOKEN_38 = 38; //"+"
-//        int t_TOKEN_39 = 39; //"-"
-//        int t_TOKEN_40 = 40; //"*"
-//        int t_TOKEN_41 = 41; //"/"
-//        int t_TOKEN_42 = 42; //"%"
-//        int t_TOKEN_43 = 43; //"!="
-//        int t_TOKEN_44 = 44; //"&&"
-//        int t_TOKEN_45 = 45; //"||"
-//        int t_TOKEN_46 = 46; //"!"
-//        int t_TOKEN_47 = 47; //"++"
-//        int t_TOKEN_48 = 48; //"--"
     }
 }
